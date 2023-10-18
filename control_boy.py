2644b57047a1ac5e89febdab60bc699cf8464d03
JeongJinbong <jbong0104@naver.com>
2023-10-18 18:46:16 +0900

    init

---
+++ control_boy.py
@@ -0,0 +1,61 @@
+from pico2d import *
+
+import game_world
+from boy import Boy
+from grass import Grass
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global boy
+
+    running = True
+
+    front_grass = Grass()
+    back_grass = Grass(400, 50)
+
+    game_world.add_object(back_grass,0)
+    game_world.add_object(front_grass,2)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+
+def update_world():
+    game_world.update()
+
+
+def render_world():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
